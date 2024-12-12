80c51fdfd41b35e2bf8b4de303a407f428ea9f36
jjnsiah <jjnsiah@gmail.com>
2024-12-12 03:44:54 +0000

    add curl command to bash script

--- monitor.sh
+++ monitor.sh
@@ -2,8 +2,13 @@
 
 CONTAINER_NAME="assessment-web-app"
 
+# check if container is running, wait for 20 seconds and check if app is accessible via localhost
 if docker ps --format '{{.Names}}' | grep -q "$CONTAINER_NAME"; then
   echo "Container '$CONTAINER_NAME' is running."
+
+  sleep 20
+  curl --fail http://localhost:3000 || exit 1
+
 else
   echo "Container '$CONTAINER_NAME' is not running."
 fi
